1f42415f1a3f6f58a742d1a135f8db34a73cb7df
Alina Gainanova <alinagaripova@MacBook-Pro-Alina.local>
2020-12-17 18:22:11 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "voting-gatsby",
+  },
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
